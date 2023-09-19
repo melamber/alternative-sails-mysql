026eacbb86499f67da090f7ee0d75cd467fd4c62
Anton Shramkov <anton.shramkov@gmail.com>
2023-09-19 11:19:57 +0300

    Debug connection

--- helpers/private/connection/spawn-connection.js
+++ helpers/private/connection/spawn-connection.js
@@ -64,7 +64,7 @@ module.exports = function spawnConnection(datastore, cb) {
     } catch (error) {
       console.log(JSON.stringify({
         event: 'sql_caught',
-        error,
+        error: error.message,
       }));
     }
   };
