cb14176a72d3f44b7c7765483f64e1f86da29d9c
Anton Shramkov <anton.shramkov@gmail.com>
2023-09-20 11:42:44 +0300

    Updated spawn-connection

--- helpers/private/connection/spawn-connection.js
+++ helpers/private/connection/spawn-connection.js
@@ -16,81 +16,77 @@
 
 var MySQL = require('machinepack-mysql');
 
+function chooseDataStore(dataStore) {
+  const dataStorePool = dataStore.manager.pool;
+  const dataStoreAllConnectionsLength = dataStorePool._allConnections.length;
+  const dataStoreFreeConnectionsLength = dataStorePool._freeConnections.length;
 
-module.exports = function spawnConnection(datastore, cb) {
-  const chooseDataStore = (dataStore) => {
-    const dataStorePool = dataStore.manager.pool;
-    const dataStoreAllConnectionsLength = dataStorePool._allConnections.length;
-    const dataStoreFreeConnectionsLength = dataStorePool._freeConnections.length;
+  if (!dataStore.alternative || dataStoreAllConnectionsLength < 1) {
+    return dataStore;
+  }
 
-    if (!dataStore.alternative || dataStoreAllConnectionsLength < 1) {
-      return dataStore;
-    }
+  const alternativeStorePool = dataStore.alternative.manager.pool;
+  const alternativeStoreConnectionLimit = alternativeStorePool.config.connectionLimit;
+  const alternativeStoreAllConnectionsLength = alternativeStorePool._allConnections.length;
+  const alternativeStoreFreeConnectionsLength = alternativeStorePool._freeConnections.length;
 
-    const alternativeStorePool = dataStore.alternative.manager.pool;
-    const alternativeStoreConnectionLimit = alternativeStorePool.config.connectionLimit;
-    const alternativeStoreAllConnectionsLength = alternativeStorePool._allConnections.length;
-    const alternativeStoreFreeConnectionsLength = alternativeStorePool._freeConnections.length;
+  if (dataStoreFreeConnectionsLength >= alternativeStoreFreeConnectionsLength
+    || (
+      (alternativeStoreAllConnectionsLength - alternativeStoreFreeConnectionsLength)
+      >= alternativeStoreConnectionLimit
+    )) {
+    return dataStore;
+  }
 
-    if (dataStoreFreeConnectionsLength >= alternativeStoreFreeConnectionsLength
-      || (
-        (alternativeStoreAllConnectionsLength - alternativeStoreFreeConnectionsLength)
-        >= alternativeStoreConnectionLimit
-      )) {
-      return dataStore;
-    }
-
-    const alternative = {
-      ...dataStore.alternative,
-      sourceDataStore: dataStore,
-    };
-
-    return chooseDataStore(alternative);
+  const alternative = {
+    ...dataStore.alternative,
+    sourceDataStore: dataStore,
   };
-  const getConnection = (dataStore, cb) => {
-    // Validate datastore
-    if (!dataStore || !dataStore.manager || !dataStore.config) {
-      return cb(new Error('Spawn Connection requires a valid datastore.'));
-    }
 
-    MySQL.getConnection({
-      manager: dataStore.manager,
-      meta: dataStore.config
-    })
-      .switch({
-        error(err) {
-          return cb(err);
-        },
-        failed(err) {
-          if (dataStore.alternative) {
-            return getConnection(dataStore.alternative, cb);
-          }
+  return chooseDataStore(alternative);
+}
 
-          if (dataStore.sourceDataStore) {
-            return getConnection({
-              ...dataStore.sourceDataStore,
-              alternative: undefined,
-            }, cb);
-          }
+function getConnection(dataStore, cb) {
+  // Validate datastore
+  if (!dataStore || !dataStore.manager || !dataStore.config) {
+    return cb(new Error('Spawn Connection requires a valid datastore.'));
+  }
 
-          return cb(err);
-        },
-        success(connection) {
-          return cb(null, connection.connection);
+  MySQL.getConnection({
+    manager: dataStore.manager,
+    meta: dataStore.config
+  })
+    .switch({
+      error(err) {
+        return cb(err);
+      },
+      failed(err) {
+        // failed to connect
+        if (dataStore.alternative) {
+          return getConnection(dataStore.alternative, cb);
         }
-      });
-  };
+
+        if (dataStore.sourceDataStore) {
+          return getConnection({
+            ...dataStore.sourceDataStore,
+            alternative: undefined,
+          }, cb);
+        }
+
+        return cb(err);
+      },
+      success(connection) {
+        return cb(null, connection.connection);
+      }
+    });
+}
+
+function spawnConnection(datastore, cb) {
   const dataStore = chooseDataStore(datastore);
 
-  return getConnection(dataStore, (err, conn) => {
-    if (err) {
-      if  (!Object.is(dataStore, datastore)) {
-        return getConnection(datastore, cb);
-      } else {
-        return cb(err);
-      }
-    }
+  return getConnection(dataStore, cb);
+}
 
-    cb(null, conn);
-  });
-};
+module.exports = spawnConnection;
+
+
